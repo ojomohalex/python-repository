52d8c6acac7af7af4848d9eacc8db7e8ad0d202f
EC2 Default User <ec2-user@ip-10-0-0-79.ec2.internal>
2022-05-27 12:02:39 +0000

    week 15A

---
+++ week15 projectA.py
@@ -0,0 +1,17 @@
+"""
+Your module description
+"""
+import boto3
+
+ec2_client=boto3.client('ec2')
+
+x=ec2_client.describe_instances()
+
+data=x["Reservations"]
+li=[]
+for instances in data:
+    instance=instances["Instances"]
+    for ids in instance:
+        instance_id=ids["InstanceId"]
+        li.append(instance_id)
+ec2_client.stop_instances(InstanceIds=li)
